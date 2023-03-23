300c5c86fde7975c254d72318dfb1e9c0ef023ec
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-03-23 09:11:21 -0700

    Checking to return if upper or lower

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -3,14 +3,14 @@
 /**
  *_isupper : checking with char c
  * c : to check and return
- * Return: 1 if c is uppercase or return 0 otherwise.
+ * Return: 1 if c _isupper case or return 0 otherwise.
  */
 int _isupper(int c)
 {
-        if (c >= 'A' && c <= 'Z')
-        {
-                return (1);
-        }
-        else
-                return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+		return (0);
 }
