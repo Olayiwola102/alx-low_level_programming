17b121a0fd0cf76775f102b1bf3a2df4588764f6
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-03-23 10:53:47 -0700

    2-mul task

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * print_numbers - printing numbers from 0 - 9
+ * Return: void(nothing)
+ */
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+	putchar (i);
+	}
+	_putchar ('\n');
+}
