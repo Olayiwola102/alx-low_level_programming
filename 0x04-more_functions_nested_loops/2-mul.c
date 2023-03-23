655eabeef4f7cfca164b87c2b51270e3c7fd0352
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-03-23 10:10:40 -0700

    1-digit task

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * mul - multiplication of two numbers
+ * @a: int value to multiply b
+ * @b: int value to multiply a
+ * Return: the multiplication result after operation
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
