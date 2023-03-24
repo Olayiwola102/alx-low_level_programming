6656ba58edf0306ff6aa5b56c1cc20f859b86779
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-03-23 17:10:22 -0700

    task 12

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,32 +1,28 @@
-#include <stdio.h>
-#include <math.h>
 #include "main.h"
+
 /**
  * main - prints the largest prime factorial of 612852475143
  *
  * Return: 0
  */
-int main(void)
+void print_number(int n)
 {
-	long int max = -1, n = 612852475143;
-	int i = 3;
+	unsigned int n1;
 
-	while (n % 2 == 0)
+	if (n < 0)
 	{
-		max = 2;
-		n /= 2;
+		n1 = -n;
+		_putchar('-');
 	}
-	while (i <= sqrt(n))
+	else
 	{
-		while (n % i == 0)
-		{
-			max = i;
-			n = n / i;
-		}
-		i += 2;
+		n1 = n;
 	}
-	if (n > 2)
-		max = n;
-	printf("%ld\n", max);
-	return (0);
+
+	if (n1 / 10)
+	{
+		print_number(n1 / 10);
+	}
+
+	_putchar((n1 % 10) + '0');
 }
--- 0x04-more_functions_nested_loops/main.h
+++ 0x04-more_functions_nested_loops/main.h
@@ -27,4 +27,5 @@ void print_triangle(int size);
 
 void print_number(int n);
 
+
 #endif /* MAIN_H */
