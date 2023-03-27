b63d5811accfea66d1de11ce293efc8bea644223
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-03-27 10:20:57 -0700

    7-puts_half.c task

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * puts_half - prints half of a string
+ * followed by a new line
+ * @str: string to be printed
+ */
+void puts_half(char *str)
+{
+	int longi = 0;
+
+	while (*s != '\0')
+	{
+		longi++;
+		s++;
+	}
+	return (longi);
+}
