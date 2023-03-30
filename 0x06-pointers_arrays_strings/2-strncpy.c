d76529b5b4bbe948a62c882305554ee32c61b0eb
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-03-30 13:57:26 -0700

    task 2

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -5,8 +5,8 @@
  *	by making use of n  bytes from string src into dest.
  * @dest: input .
  * @src: The source string.
- * @src: input value  src.
- * Retur: returning dest.
+ * @j: input value  src.
+ * Return: returning dest.
  */
 
 char *_strncpy(char *dest, char *src, int j)
