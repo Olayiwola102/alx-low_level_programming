96c0e9f983fc71c65d9d0523c73053b5d9efc348
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-04 16:40:41 -0700

    1 task

---
+++ 0x07-pointers_arrays_strings/1-memcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _memcpy - this works by copying memory address
+ * @dest: this is the addres where its stored
+ * @src: it copy from this error
+ * *@n: these are the bytes
+ *
+ * Return: give output
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int var = 0;
+	int a = b;
+
+	for (; var < 1; var++)
+	{
+		dest[var] = src[var];
+		b = b + 1;
+	}
+	return (dest);
+}
