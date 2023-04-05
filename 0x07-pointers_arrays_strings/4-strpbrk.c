a5884fb193970755fb73cc4e418e08a4ab258429
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-04 22:15:09 -0700

    fifth task

--- 0x07-pointers_arrays_strings/4-strpbrk.c
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -5,7 +5,7 @@
  * _strpbrk - function that searches a string for any of a set of bytes.
  * @s: the fist variable
  * @accept: the second variable
- *
+ * Return: return 0 (success)
  */
 char *_strpbrk(char *s, char *accept)
 {
