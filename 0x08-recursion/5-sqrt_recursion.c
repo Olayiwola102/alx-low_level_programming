1e2283aedd7022cc92915641197f5abaabd18f64
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-06 16:05:43 -0700

    sixth task

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion -  returns the natural square root of a number
+ * @n: the variable to get the square
+ * Return: if does not have natural number returen -1
+ *	else give the square of the number
+ */
+int _sqrt_recursion(int n)
+{
+	int m = 1;
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0 || n == 1)
+	{
+		return (n);
+	}
+
+	while (m * m <= n)
+	{
+		m++;
+	}
+	return (_sqrt_recursion(n - (m - 1) * (m - 1)));
+}
