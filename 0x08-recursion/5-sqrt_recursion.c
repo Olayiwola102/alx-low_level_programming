1cc769f4af3daa0f1d085c9cd2abec1fd82eea83
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-06 19:08:29 -0700

    sixth task

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -3,49 +3,33 @@
 /**
  * _sqrt_recursions -  returns the natural square root of a number
  * @n: the variable to get the square
- * @small: small variable
- * @big: big variable
+ * @sqr: value used
  * Return: if does not have natural number returen -1
  *	else give the square of the number
  */
 
-int _sqrt_recursions(int n, int small, int big)
+int _sqrt_recursions(int n, int sqr)
 {
-	int sqr, interval;
-
-	if (small > big)
+	if (sqr * sqr == n)
+	{
+		return (sqr);
+	}
+	if (sqr * sqr > n)
 	{
 		return (-1);
 	}
-
-	interval = (small + big) / 2;
-
-	sqr = interval * interval;
-
-	if (sqr == n)
-	{
-		return (interval);
-	}
-	if (sqr < n)
-	{
-		return (_sqrt_recursions(n, interval + 1, big));
-	}
-	else
-	{
-		return (_sqrt_recursions(n, small, interval - 1));
-	}
+	return (_sqrt_recursions(n, sqr + 1));
 }
 /**
- * _sqrt_recursion - recursive square root of a number
- * @n: value
+ * _sqrt_recursion - returns the natural square root of a number
+ * @n: variable used
  * Return: return the square root
  */
 int _sqrt_recursion(int n)
 {
-
 	if (n < 0)
 	{
 		return (-1);
 	}
-	return (_sqrt_recursions(n, 0, n));
+	return (_sqrt_recursions(n, 0));
 }
