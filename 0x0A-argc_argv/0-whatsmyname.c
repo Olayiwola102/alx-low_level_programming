58313e0ec0653f94e0254e9bba4cd462cb40cf17
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-10 15:40:21 -0700

    first task

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -1,10 +1,14 @@
+#include "main.h"
 #include <stdio.h>
 
 /**
  * main - whqat my name
+ * @argc: number of arguments in the program
+ * @argv: array of arguments in the proogram
  */
 int main(int argc, char *argv[])
 {
+	(void)argc;
 	printf("%s\n", argv[0]);
 	return (0);
 }
