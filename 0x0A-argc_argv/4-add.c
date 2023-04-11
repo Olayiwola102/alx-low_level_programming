241899f35de0e96be6210c3f3f57f5dd7c401d5f
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-10 21:34:34 -0700

    fouth task

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - adds positive numbers
+ *
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: int
+ */
+int main(int argc, char *argv[])
+{
+
