f150d55bb0ef53d45180128a97fa0b0f8d798c68
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-13 14:50:38 -0700

    zero task

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - allocate memory using malloc
+ *
+ * @b: size of memory
+ *
+ * Return: memory
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
