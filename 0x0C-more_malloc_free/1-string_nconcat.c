6ee9ecad098e6aa93af16c0b9383bda788fadf14
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-13 15:00:29 -0700

    zero task

---
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * string_nconcat - concatenates n number of s1 to s2
+ *
+ * @s1: first string
+ * @s2: second string
+ * @n: number of s2 to concatenate
+ *
+ * Return: s1_s2
+ */
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+
