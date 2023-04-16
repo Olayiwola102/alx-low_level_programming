0e59071435587b0be974b05a18d01202607558e5
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-15 21:53:12 -0700

    ew add

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -1,61 +1,53 @@
-#include "main.h"
-#include <stdlib.h>
 #include <stdio.h>
-#include <ctype.h>
+#include <stdlib.h>
 
 /**
- * multiply - determines if any number is zero
- * @num1: argument vector.
- * @num2: argument
- * main - validate input and check the argument.
- * @argv - argument
- * @argc - argument
- * isNumeric - to check if a string contains only digit
- * @str - argument
- * Return: no return.
+ * _isdigit - checks digit
+ *
+ * @s: string to check
+ *
+ * Return: 1 if digit, 0 if not
  */
-unsigned int multiply(unsigned long long int num1, unsigned long long int num2)
+int _isdigit(char *s)
 {
-	return (num1 * num2);
-}
-/**
- * isNumeric - validate input
- * @str: string
- * Return: 0
- */
-int isNumeric(const char *str)
-{
-	for (int i = 0; str[i]; i++)
+	int count = 0;
+	while (*(s + count))
 	{
-		if (!isdigit(str[i]))
-		{
-			return (0);
-		}
+		if (*(s + count) > '9' || *(s + count) < '0')
+		return (0);
+		count++;
 	}
 	return (1);
 }
+
 /**
- * main - check for correct argument
- * @argv: argument
- * @argc: argument
- * Return: 0
+ * main - prints multiple of to numbers
+ *
+ * @argc: number of argument
+ * @argv: arguments passed
+ *
+ * Return: 0 on success
  */
 int main(int argc, char *argv[])
-	{
-		if (argc != 3)
-		{
-			printf("Error\n");
-			return (98);
-		}
-		if (!isNumeric(argv[1]) || !isNumeric(argv[2]))
-		{
-			printf("Error\n");
-			return (98);
-		}
-		unsigned long int num1 = strtoull(argv[1], NULL, 10);
-		unsigned long int num2 = strtoull(argv[2], NULL, 10);
-		unsigned long int result = multiply(num1, num2);
+{
+	char num1, num2;
+	unsigned int res;
 
-		printf("%llu\n", result);
-		return (0);
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
 	}
+	
+	if (!_isdigit(argv[1]) || !_isdigit(argv[2]))
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	
+	num1 = atoi(argv[1]);
+	num2 = atoi(argv[2]);
+	res = num1 * num2;
+	printf("%d\n", res);
+	return (0);
+}
