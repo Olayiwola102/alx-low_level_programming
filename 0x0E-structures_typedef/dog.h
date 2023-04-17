50346086e7f8cc8ea9643d84717816bae1f93ee7
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-17 09:25:43 -0700

    task zero

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - is the dog informations
+ * @name: is the dog name
+ * @age: is the dog`s age
+ * @owner: the dog`s owner
+ */
+
+struct dog {
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
