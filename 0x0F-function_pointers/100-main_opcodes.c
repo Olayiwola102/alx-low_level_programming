db6da5d44c524b37dc3a77183ea3be48f010a7b8
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-19 21:43:28 -0700

    last task

---
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program that prints the opcodes of its own main function
+ *
+ * @argc: count of args
+ * @argv: array of args
+ *
+ * Return: int
+ */
+
+int main(int argc, char **argv)
+{
+	int n, bytes;
+	char *addr;
+	
+	if (argc != 2)
+	{
+		printf("Error\n");
+		exit(1);
+	}
+	bytes = atoi(argv[1]);
+	if (bytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+	addr = (char *)main;
+	for (n = 0; n < bytes - 1; n++)
+		printf("%02hhx ", addr[n]);
+	printf("%02hhx\n", addr[n]);
+	return 
+}
