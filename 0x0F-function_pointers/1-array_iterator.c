74b82998127f9627ca021a98554a046352d20e5f
Abdulwaheed Musa <abdulwaheedmusa101@gmail.com>
2023-04-19 21:15:04 -0700

    error checked

--- 0x0F-function_pointers/1-array_iterator.c
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -9,6 +9,11 @@
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
+	unsigned int i;
+
+	if (array == NULL || action == NULL)
+		return;
+
 	for (size_t i = 0; i < size; i++)
 	{
 		action(array[i]);
